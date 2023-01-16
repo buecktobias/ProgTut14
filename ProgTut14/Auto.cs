3eb79b3bd50e4ab8f5205aa4cb8a040f82de57a0
Tobias Bück <buecktobias@gmail.com>
2023-01-16 18:47:28 +0100

    init

--- ProgTut14/Auto.cs
+++ ProgTut14/Auto.cs
@@ -1,6 +1,105 @@
-﻿namespace ProgTut14;
+﻿using System.ComponentModel;
+
+namespace ProgTut14;
+
 
 public class Auto
 {
-    
+    private Motor _motor;
+
+    public double PS
+    {
+        get => _motor.Ps;
+    }
+
+    public Autotyp GetAutotyp()
+    {
+        if (_motor is ElektroMotor)
+        {
+            return Autotyp.Elektro;
+        }else if (_motor is BenzinMotor)
+        {
+            return Autotyp.Benzin;
+        }
+        else
+        {
+            throw new InvalidEnumArgumentException("Autotyp ist nicht vorhanden!!");
+        }
+    }
+
+    public void Tanken(Kraftstoff kraftstoff)
+    {
+        if (kraftstoff == Kraftstoff.Benzin)
+        {
+            if (GetAutotyp() == Autotyp.Benzin)
+            {
+                Console.WriteLine("Auto wird getankt!");
+            }
+            else
+            {
+                throw new WrongKraftstoffException();
+            }
+        }
+        else if (kraftstoff == Kraftstoff.Strom)
+        {
+            if (GetAutotyp() == Autotyp.Elektro)
+            {
+                Console.WriteLine("Auto wird getankt!");
+            }
+            else
+            {
+                throw new WrongKraftstoffException();
+            }
+        }
+    }
+
+    public Auto(Motor motor)
+    {
+        _motor = motor;
+    }
+
+    public override string ToString()
+    {
+        return $"Das Auto hat: {nameof(_motor)}: {_motor}, {nameof(PS)}: {PS}";
+    }
+}
+
+public class WrongKraftstoffException: InvalidOperationException{}
+
+public abstract class Motor
+{
+    public double Ps { get; }
+
+    protected Motor(double ps)
+    {
+        if (ps <= 0)
+        {
+            throw new ArgumentException("PS muss größer als 0 sein");
+        }
+        Ps = ps;
+    }
+}
+
+public class ElektroMotor : Motor
+{
+    public ElektroMotor(double ps) : base(ps)
+    {
+    }
+}
+
+public class BenzinMotor : Motor
+{
+    public BenzinMotor(double ps) : base(ps)
+    {
+    }
+}
+
+public enum Kraftstoff
+{
+    Benzin, Strom
+}
+
+public enum Autotyp
+{
+    Elektro, Benzin
 }
--- ProgTut14/Program.cs
+++ ProgTut14/Program.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using ProgTut14;
+
+
+var mercedes = new Auto(new BenzinMotor(100));
+var tesla = new Auto(new ElektroMotor(200));
+Console.WriteLine(mercedes);
+Console.WriteLine(tesla);
+
+mercedes.Tanken(Kraftstoff.Benzin);
+try
+{
+    mercedes.Tanken(Kraftstoff.Strom);
+}
+catch (WrongKraftstoffException _)
+{
+    Console.WriteLine("Der Mercedes kann nicht mit Strom getankt werden");
+}
+
+tesla.Tanken(Kraftstoff.Strom);
+try
+{
+    tesla.Tanken(Kraftstoff.Benzin);
+}
+catch (WrongKraftstoffException _)
+{
+    Console.WriteLine("Der Tesla kann nicht mit Strom getankt werden");
+}
+
+Console.WriteLine($"Mercedes ist ein {mercedes.GetAutotyp()}");
+Console.WriteLine($"Tesla ist ein {tesla.GetAutotyp()}");
